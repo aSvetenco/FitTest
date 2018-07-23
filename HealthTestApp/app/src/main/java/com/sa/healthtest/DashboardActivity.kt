389b6895cf38fc8bc1cfc2a646e8d50be5f710e7
Alexandr Svetenco <asvetenco@gmail.com>
2018-07-24 00:52:20 +0300

    ui/added ui elements

--- HealthTestApp/app/src/main/java/com/sa/healthtest/DashboardActivity.kt
+++ HealthTestApp/app/src/main/java/com/sa/healthtest/DashboardActivity.kt
@@ -5,12 +5,20 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.Toolbar
+import android.view.View
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.fitness.FitnessOptions
 import com.google.android.gms.fitness.data.DataSet
+import com.sa.healthtest.R.string.retrieve
 import com.sa.healthtest.connect.ConnectCallback
 import com.sa.healthtest.connect.GoogleFitConnectService
 import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 class DashboardActivity : AppCompatActivity(), ConnectCallback {
 
@@ -20,13 +28,43 @@ class DashboardActivity : AppCompatActivity(), ConnectCallback {
 
     private val GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 9991
     private lateinit var service: GoogleFitConnectService
+    private lateinit var navMenu: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionbar: ActionBar? = supportActionBar
+        navMenu = findViewById(R.id.nav_menu)
+        initNavDrawer()
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+            title = ""
+        }
         service = GoogleFitConnectService(this)
-        google_fit.setOnClickListener { service.connectToGoogleFit()}
-        retrieve.setOnClickListener{service.retrieveData()}
+
+    }
+
+    fun initNavDrawer() {
+        val toggle = object : ActionBarDrawerToggle(this, navMenu, toolbar, R.string.nav_open, R.string.nav_close) {
+            override fun onDrawerClosed(view: View) {
+                super.onDrawerClosed(view)
+
+            }
+
+            override fun onDrawerOpened(drawerView: View) {
+                super.onDrawerOpened(drawerView)
+
+            }
+        }
+        toggle.apply {
+            isDrawerIndicatorEnabled = true
+            setToolbarNavigationClickListener { navMenu.openDrawer(GravityCompat.START) }
+        }
+        navMenu.addDrawerListener(toggle)
+        toggle.syncState()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -51,9 +89,9 @@ class DashboardActivity : AppCompatActivity(), ConnectCallback {
     }
 
     override fun successConnected() {
-        google_fit.text = getString(R.string.connected)
-        google_fit.setCompoundDrawablesWithIntrinsicBounds(
-                ContextCompat.getDrawable(this, R.drawable.ic_check),null, null, null)
+        /*     google_fit.text = getString(R.string.connected)
+             google_fit.setCompoundDrawablesWithIntrinsicBounds(
+                     ContextCompat.getDrawable(this, R.drawable.ic_check),null, null, null)*/
     }
 
     override fun error() {
