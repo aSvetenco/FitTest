a59b1385734dd487539f91493fcecf0fdcb2fb00
Alexandr Svetenco <asvetenco@endava.com>
2018-07-26 12:07:42 +0300

    samsung_fit/added sdk

---
+++ HealthTestApp/app/src/main/java/com/sa/healthtest/services/SamsungHealthService.java
@@ -0,0 +1,15 @@
+package com.sa.healthtest.services;
+
+import com.samsung.android.sdk.healthdata.HealthDataObserver;
+
+public class SamsungHealthService implements FitConnection {
+
+
+    @Override
+    public void checkPermission() {
+    }
+
+    @Override
+    public void disconnect() {
+    }
+}
